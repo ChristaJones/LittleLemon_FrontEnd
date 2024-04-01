c49490844382e280311bb633244f4de321196194
ChristaJones <35708193+ChristaJones@users.noreply.github.com>
2024-04-01 00:48:16 -0400

    Update Nav.js

--- littlelemon/src/components/Nav.js
+++ littlelemon/src/components/Nav.js
@@ -2,26 +2,27 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from "../images/Logo.svg";
-
+import { Link } from 'react-router-dom';
 const NavSection = () => {
+	//const {availableTimesByDate, setAvailableTimesByDate} = useDate()
   return (
   <div>
  
    		  	
-  <Navbar expand="lg" className="bg-body-tertiary">
+  <Navbar expand="lg" className="bg-body-tertiary" aria-label="main-navigation">
       <Container>
-      <Navbar.Brand href="/home">
+      <Navbar.Brand as={Link} to="/home">
             <img src={Logo} alt="logo"/>
           </Navbar.Brand>
 	        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 		<Navbar.Collapse id="basic-navbar-nav">
 			<Nav className="me-auto">
-			      <Nav.Link href="/home">Home</Nav.Link>
-		              <Nav.Link href="#about">About</Nav.Link>
-		              <Nav.Link href="#menu">Menu</Nav.Link>
-		              <Nav.Link href="/reservations">Reservations</Nav.Link>
-		              <Nav.Link href="#order">Order Online</Nav.Link>
-		              <Nav.Link href="#login">Login</Nav.Link>
+			      <Nav.Link as={Link} to="/home">Home</Nav.Link>
+		              <Nav.Link as={Link} to="#about">About</Nav.Link>
+		              <Nav.Link as={Link} to="#menu">Menu</Nav.Link>
+		              <Nav.Link as={Link} to="/reservations">Reservations</Nav.Link>
+		              <Nav.Link as={Link} to="#order">Order Online</Nav.Link>
+		              <Nav.Link as={Link} to="#login">Login</Nav.Link>
 		      </Nav>
 	      </Navbar.Collapse>
       </Container>
@@ -31,5 +32,6 @@ const NavSection = () => {
   );
 };
 export default NavSection;
+   
    
             
