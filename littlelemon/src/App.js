22d084e1322bf7ac8ea4b1f791ce06fa661110cc
ChristaJones <35708193+ChristaJones@users.noreply.github.com>
2024-04-01 00:51:04 -0400

    Update App.js

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Layout from "./components/Layout";
 import Main from "./components/Main";
 import BookingPage from "./components/BookingPage";
 import ConfirmationPage from "./components/ConfirmationPage";
@@ -9,16 +8,18 @@ import ConfirmationPage from "./components/ConfirmationPage";
 import { Routes, Route} from "react-router-dom";
 
 function App() {
+
   return (
+  
    <div >
-	<Header />
-	  <Routes>
-        <Route path="/" element={<Main />}></Route>
-    	<Route path="/home" element={<Main />}></Route>
-        <Route path="/reservations" element={<BookingPage />}></Route>
-        <Route path="/confirmation" element={<ConfirmationPage />}></Route>
-       </Routes>
-	<Footer />
+	<Routes>
+		<Route path="/" element={<Layout />}>
+			<Route index element={<Main />}/>		     
+		    	<Route path="home" element={<Main />}/>
+		        <Route path="reservations" element={<BookingPage />}/>
+		        <Route path="confirmation" element={<ConfirmationPage />}/>
+		</Route>
+        </Routes>
     </div>
 
   );
